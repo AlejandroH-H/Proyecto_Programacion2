d849708779e8f19a516b94d3b9eb8b5f6286c0d4
Alejandro <161656941+AlejandroH-H@users.noreply.github.com>
2024-06-12 21:43:45 -0400

    Acá modifiqué el repositorio del proyecto con Express Generator, para que tengamos una base para empezar con todos los ajustes listos en la app...

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
+const createError = require('http-errors');
 const express = require('express');
-const app= express();
-const inicioRouter = require('./src/routes/inicio.js');
 const path = require('path');
-const morgan = require('morgan')
-require('ejs')
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+//Rutas Principales
+app.use(indexRouter);
+app.use(usersRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.json());
-app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.get('/',inicioRouter);
-
-app.listen(3000)
-console.log('localhost iniciado en el puerto 3000')
+module.exports = app;
