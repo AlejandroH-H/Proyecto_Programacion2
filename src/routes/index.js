6247917e2ae178b4b1dd6e80e5a613c017e390b4
Alejandro <161656941+AlejandroH-H@users.noreply.github.com>
2024-06-15 18:05:16 -0400

    Eliminar el objeto de 'title' que por los momentos no tiene funcionamiento para el 'index.ejs'

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,25 +3,25 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('pages/index', { title: 'Express' });
+  res.render('pages/index')
 });
 
 router.get('/inicio', (req, res) => {
-  res.render('pages/inicioDeSesion', { title: 'Express' });
+  res.render('pages/inicioDeSesion')
 });
 
 router.get('/TerminosCondiciones', (req, res) => {
-  res.render('pages/terminos', { title: 'Express' });
+  res.render('pages/terminos')
 });
 
 router.get('/home', (req, res) => {
-  res.render('pages/home', { title: 'Express' });
+  res.render('pages/home')
 });
 router.get('/datos',(req,res)=>{
-  res.render('pages/datos', { title: 'Express' });
+  res.render('pages/datos')
 })
 router.get('/preEleccion',(req,res)=>{
-  res.render('pages/preEleccionMateria', { title: 'Express' });
+  res.render('pages/preEleccionMateria')
 })
 
 
